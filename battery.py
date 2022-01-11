a6589a93b47af901d48e8fac6d8ab0ecb6502af2
Cees Paris <ceesparis@Ceess-MacBook-Pro.local>
2022-01-11 14:17:10 +0100

    testobjects

---
+++ battery.py
@@ -0,0 +1,7 @@
+class Battery():
+    
+    def __init__(self, location, power, houses):
+        self.location = ()
+        self.power = 0
+        self.houses = []
+    
---
+++ house.py
@@ -0,0 +1,5 @@
+class House(): 
+    def __init__(self, location, power, cables):
+        self.location = ()
+        self.power = 0
+        self.cables = []
