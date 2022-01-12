15943dd5e0e02e62fe47844e1b4978bfff83c567
Cees Paris <ceesparis@Ceess-MacBook-Pro.local>
2022-01-12 09:44:36 +0100

    firstgridwithcomments

--- loader.py
+++ loader.py
@@ -5,27 +5,34 @@ from house import House
 
 def load_grid(dis_number):
 
+    # make a list to store batteries and housess from csv in 
     batteries = []
     houses = []
 
+    # open the batteries-csv from the district you want to represent
     with open(f'./Huizen&Batterijen/district_{dis_number}/district-{dis_number}_batteries.csv', 'r') as f:
         csv_reader = csv.reader(f)
+        # skip the first row with the row-names
         next(csv_reader)
+        # make battery objects from the remaining rows
         for line in csv_reader:
             location = line[0].strip('')
             power = line[1]
             new_battery = Battery(location, power)
             batteries.append(new_battery)
     
+    # open the housess-csv from the district you want to represent
     with open(f'./Huizen&Batterijen/district_{dis_number}/district-{dis_number}_houses.csv', 'r') as f:
         csv_reader = csv.reader(f)
+        # skipt the first row with the row-names
         next(csv_reader)
+        # make house objects from the remaining rows
         for line in csv_reader:
             location = (int(line[0]), int(line[1]))
             power = line[2]
             new_house = House(location, power)
             houses.append(new_house)
 
+    # return a list containing all made objects 
     grid = [batteries, houses]
-    print(grid)
     return grid
--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import loader
 
 class Grid():
     def __init__(self, district):
-        self.grid = loader.load_grid(district)
+        # load the information from relevant district 
+        grid = loader.load_grid(district)
+        # add batteries of district to grid
+        self.batteries = grid[0]
+        # add houses of district to grid
+        self.houses = grid[1]
 
 if __name__ == "__main__":
     test_district = 1
     grid = Grid(test_district)
-print(grid)
+print(grid.batteries)
