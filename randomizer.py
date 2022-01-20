e227d6cad7b34c94fe86666db98e3fc3cb085524
Cees <ceesparis1@live.nl>
2022-01-20 14:52:16 +0100

    randomizer10k

--- randomizer.py
+++ randomizer.py
@@ -1,8 +1,6 @@
 import random
 import copy
-from grid import Grid
 from costCalculation import calculateCost
-from visualization import visualize
 
 class Randomizer():
 
@@ -14,13 +12,12 @@ class Randomizer():
         self.batteries = grid.batteries
 
 
-
     def random_cables(self):
         '''
         Copies lists of houses and batteries.
         Randomnly connects houses to batteries and
-        returns these randomly connected houses and batteries as a list.
-        NB works but heavy on the deepcopy!
+        if resulting grid is valid, returns the cost of 
+        this configuration. Else returns None. 
         '''
 
         houses = copy.deepcopy(self.houses)
@@ -65,53 +62,44 @@ class Randomizer():
             house.add_cable(x_start, y_start)
             battery.drain(house.output)
 
-        # return a list with the connected houses and drained batteries
-        ran_grid_info = [houses, batteries]
-        return ran_grid_info
+        # calculate cost of valid grid and return it
+        cost = calculateCost(houses, len(batteries))
+        return cost
 
 
     def multiple_random(self):
         '''
         Calls random_cables function for as long as is needed to get
         x amount of valid random configurated grids.
-        Returns list of valid random grids.
+        Returns list wth total cost and number of grids.
         '''
-        # create a list to store grids
-        suc_rand_grids = []
+        # add up grid costs in total
+        total = 0
+        # keep track of how many grids we currently have in current_grids
+        current_grids = 0
         # specify number of grids
-        number_of_grids = 100
+        number_of_grids = 10000
         # while this number is not yet reached, make random configurations and keep them if they are valid
-        while len(suc_rand_grids) < number_of_grids:
+        while current_grids < number_of_grids:
             valid = True
             random_grid = self.random_cables()
             # if nothing is returned, the grid made was invalid
             if random_grid == None:
                 valid = False
-            # if the grid is valid, make Grid object with info of the randomly configured grid
+            # if the grid is valid, add its costs to total and add to number of current grids
             if valid == True:
-                new_grid = Grid(self.district)
-                houses = random_grid[0]
-                batteries = random_grid[1]
-                new_grid.houses = houses
-                new_grid.batteries = batteries
-                suc_rand_grids.append(new_grid)
-        # return list of valid random grids
-        return(suc_rand_grids)
+                total += random_grid
+                current_grids += 1
+        # return both the number of valid grids as well as their total cost
+        grids_info = [total, number_of_grids]
+        return grids_info
 
-    def calc_average(self, grid_list):
+    def calc_average(self, grids_info):
         '''
-        Takes in list of valid grids.
-        Prints average cost of this list of grids.
+        Takes in list of total cost and number of grids.
+        Prints average cost of these grids.
         '''
-        total = 0
-        # divide over the number of grids in the list
-        grids_count = len(grid_list)
-        # use calculate function to determine costs for each grid, then add these to total
-        for grid in grid_list:
-            batterySum = len(grid.batteries)
-            houses = grid.houses
-            cost = calculateCost(houses, batterySum)
-            total += cost
-        # divide total over quantity grids in the list
+        total = grids_info[0]
+        grids_count = grids_info[1]
         average = total / grids_count
-        print(average)
+        print(int(average))
