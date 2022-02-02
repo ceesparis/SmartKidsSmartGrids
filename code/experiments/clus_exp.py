067fe3dfb0996dd1e05994effa823ff1435f069a
Cees <ceesparis1@live.nl>
2022-02-02 18:41:58 +0100

    readmefor experiment

--- code/experiments/clus_exp.py
+++ code/experiments/clus_exp.py
@@ -1,11 +1,10 @@
 import subprocess
 import time
-from experiment import experiment
 
-start = time.time()
-n_runs = 0
-while time.time() - start < 1:
-    print(f"run: {n_runs}")
-    subprocess.call(["python3", "experiment.py"])
-    # experiment()
-    n_runs += 1
+def clus_experiment():
+    start = time.time()
+    n_runs = 0
+    while time.time() - start < 1800:
+        print(f"run: {n_runs}")
+        subprocess.call(["python3", "experiment.py"])
+        n_runs += 1
