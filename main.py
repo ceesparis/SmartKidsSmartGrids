1405a707da4b4a59948be67e7f2f311c4d323e1f
sannedb <sannemarrone@gmail.com>
2022-02-02 21:44:08 +0100

    tuned last code

--- main.py
+++ main.py
@@ -9,21 +9,26 @@ from code.classes.randomizer import Randomizer
 from code.classes.grid import Grid
 
 if __name__ == "__main__":
+    
     # check for correct usage
     if len(argv) < 3:
         print('Usage: Python3 main.py [district number] [ALGORITHM(CW or SD)]')
         exit(1)
+    
     district = argv[1]
+    
     # check if district number is valid
     try:
         grid = Grid(district)
         grid.load_from_csv()
+    
     except Exception:
         print('district not found')
         exit(2)
 
   # check if input is only letters, if so, capitalize input
     algorithm = argv[2]
+    
     if algorithm.isalpha():
         algorithm = algorithm.upper()
 
@@ -33,17 +38,21 @@ if __name__ == "__main__":
         grid = iterateDistribution(grid, loops)
         grid.printOutput()
         trying = False
+    
     # if clusterwebz is asked for, run this algorithm
     elif algorithm in "CW":
         cluster_grid = Clusalgo(grid)
         cluster_grid.cluster_houses()
         cluster_grid.connect_clusters()
         cluster_grid.update_houses()
+        
         total_shared = calculateCostShared(
             cluster_grid.houses, cluster_grid.batteries)
+        
         print(total_shared)
         visualize(cluster_grid)
         trying = False
+    
     # if not, tell the user what they should be doing better
     else:
         print("be more clear please\n")
