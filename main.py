0ab7f5286a66a4707b52a864e207b3ffc5327340
Cees <ceesparis1@live.nl>
2022-02-02 17:36:15 +0100

    better main 2 command line arguments

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from code.experiments.experiment import experiment
 
 if __name__ == "__main__":
     # check for correct usage
-    if len(argv) != 2:
-        print('Usage: Python3 main.py [district number]')
+    if len(argv) != 3:
+        print('Usage: Python3 main.py [district number] ALGORITHM(CW or SD)')
         exit(1)
     district = argv[1]
     # check if district number is valid
@@ -21,35 +21,27 @@ if __name__ == "__main__":
     except Exception:
         print('district not found')
         exit(2)
-    
-    # experiment(1)
-    visualize_exp("./data/experiments/cluster/cluster_results_district1.csv", district)
-    # ask user what algorithm they want to employ for laying the smartgrid
-    print(f"\nchoose your algorithm for district {district}: clusterWebz or smartDistribution\n")
-    algo_choice = input("Make your choice here(CW or SD): ")
-    algo_choice = algo_choice.upper()
-    trying = True
+    algorithm = argv[2]
+    if algorithm.isalpha():
+        algorithm = algorithm.upper()
 
-    #
-    while trying:
-        if algo_choice in "SD":
-            loops = 10
-            grid = iterateAstar(grid, loops)
-            grid.printOutput()
-            trying = False
-        elif algo_choice in "CW":
-            cluster_grid = Clusalgo(grid)
-            cluster_grid.cluster_houses()
-            cluster_grid.connect_clusters()
-            cluster_grid.update_houses()
-            total_shared = calculateCostShared(cluster_grid.houses, cluster_grid.batteries)
-            print(total_shared)
-            visualize(cluster_grid)
-            trying = False
-        elif algo_choice in "QUIT":
-            break
-        else:
-            print("be more clear please\n")
-            print("choose your algorithm: clusterWebz(CW), smartDistribution(SD), or Q to quit\n")
-            algo_choice = input("Make your choice here(CW or SD): ")
+
+    if algorithm in "SD":
+        loops = 10
+        grid = iterateAstar(grid, loops)
+        grid.printOutput()
+        trying = False
+    elif algorithm in "CW":
+        cluster_grid = Clusalgo(grid)
+        cluster_grid.cluster_houses()
+        cluster_grid.connect_clusters()
+        cluster_grid.update_houses()
+        total_shared = calculateCostShared(cluster_grid.houses, cluster_grid.batteries)
+        print(total_shared)
+        visualize(cluster_grid)
+        trying = False
+    else:
+        print("be more clear please\n")
+        print("choose your algorithm: clusterWebz(CW), smartDistribution(SD)\n")
+          
 
