df924b2ef3c7a384f758c71305b3a4b5b54c7308
sannedb <sannemarrone@gmail.com>
2022-02-03 13:09:49 +0100

    fixed layRoute function case

--- code/helpers/createRoute.py
+++ code/helpers/createRoute.py
@@ -1,35 +1,35 @@
-def lay_Route(house, end):
+def layRoute(house, end):
     """
     Will lay cables between the house and configured battery coords
     for as long as the end point has not been reached.
     """
 
-    begin_house = house
+    beginHouse = house
 
-    begin_x = begin_house.location[0]
-    begin_y = begin_house.location[1]
+    beginX = beginHouse.location[0]
+    beginY = beginHouse.location[1]
 
-    end_x = end.location[0]
-    end_y = end.location[1]
+    endX = end.location[0]
+    endY = end.location[1]
 
-    begin_house.add_cable(begin_x, begin_y)
+    beginHouse.addCable(beginX, beginY)
 
     # house is somewhere on the left of the end point
-    while begin_x < end_x:
-        begin_x += 1
-        begin_house.add_cable(begin_x, begin_y)
+    while beginX < endX:
+        beginX += 1
+        beginHouse.addCable(beginX, beginY)
     
     # house is somewhere on the right of the end point
-    while begin_x > end_x:
-        begin_x -= 1
-        begin_house.add_cable(begin_x, begin_y)
+    while beginX > endX:
+        beginX -= 1
+        beginHouse.addCable(beginX, beginY)
 
     # house is somewhere below the end point
-    while begin_y < end_y:
-        begin_y += 1
-        begin_house.add_cable(begin_x, begin_y)
+    while beginY < endY:
+        beginY += 1
+        beginHouse.addCable(beginX, beginY)
 
     # house is somewhere above the end point
-    while begin_y > end_y:
-        begin_y -= 1
-        begin_house.add_cable(begin_x, begin_y)
+    while beginY > endY:
+        beginY -= 1
+        beginHouse.addCable(beginX, beginY)
